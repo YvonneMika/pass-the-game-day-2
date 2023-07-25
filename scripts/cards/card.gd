beb49fa8a48c340141a589b1af45295ce0209791
Truc4 <curtreyes00@yahoo.com>
2023-07-24 23:43:25 -0600

    hand fixes

--- scripts/cards/card.gd
+++ scripts/cards/card.gd
@@ -2,3 +2,16 @@ extends Panel
 class_name Card
 ## Base card all cards inherit from
 
+@onready var anim_player := $AnimationPlayer
+
+func _on_mouse_entered() -> void:
+	anim_player.play("focus")
+	z_index = 2
+
+func _on_mouse_exited() -> void:
+	anim_player.play("focus_exit")
+	z_index = 1
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "focus_exit":
+		z_index = 0
--- scripts/cards/card_container.gd
+++ scripts/cards/card_container.gd
@@ -13,48 +13,53 @@ extends HBoxContainer
 			add_theme_constant_override("separation", separation)
 
 func _on_sort_children() -> void:
+	adjust_separation()
 	for i in range(get_child_count()):
 		var child = get_child(i)
 		
 		child.rotation_degrees = get_angle_from_x(child)
-#		child.position.y += abs(rot_deg) * 30
 		set_y_based_on_angle(child, child.rotation_degrees)
 
 func _on_child_entered_tree(node: Node) -> void:
 	node.visibility_changed.connect(_on_child_visibility_change)
-	adjust_separation()
 
 func adjust_separation():
 	var children_total_width := 0
+	var visible_children_count := 0
 	for child in get_children():
 		if !child.visible: continue
 		children_total_width += child.size.x
+		visible_children_count += 1
 	
-	print(children_total_width)
+	print("children_total_width: ", children_total_width)
 	
-	var children_width_ratio = size.x / children_total_width if children_total_width != 0 else 0 # What percent of the container the children take up
+	var children_width_ratio = float(children_total_width) / size.x if children_total_width != 0 else 0 # What percent of the container the children take up
 	
-	print(children_width_ratio)
+	print("children_width_ratio: ", children_width_ratio)
+	var child_average_width = children_total_width / visible_children_count
 	
 	# If cards take up most of this containers size
 	# overlap them based on by how much
-	if children_width_ratio > .9: 
-		separation = -children_width_ratio*50
-		print("New sep: ", -children_total_width*50)
+	if children_width_ratio > 1: 
+		separation = (size.x - children_total_width) / visible_children_count
+		print("New sep: ", separation)
 	elif separation != 0:
 		separation = 0
 		print("New sep: ", 0)
 
 func get_angle_from_x(node: Control) -> int:
 	# Angle
-	var node_x =  (node.position.x + node.size.x * 0.5) - (position.x + size.x * 0.5)
+	var node_x =  (node.position.x + node.size.x * 0.5) - (size.x * 0.5)
 	var rotation_ratio = node_x / (size.x * 0.5)
 	var rot_deg = rotation_ratio * fan_angle_degrees
 	
 	return rot_deg
 
 func set_y_based_on_angle(node: Control, theta_degrees: int):
-	var point_x = (node.position.x + node.size.x * 0.5) - (position.x + size.x * 0.5)
+	var point_x = (node.position.x + node.size.x * 0.5) - (size.x * 0.5)
+	
+	if abs(theta_degrees) < 4: # If the angle is too small the float imprecision gets out of hand (literally)
+		return 
 	
 	var theta_rad = deg_to_rad(90 - theta_degrees)
 	
@@ -66,6 +71,5 @@ func set_y_based_on_angle(node: Control, theta_degrees: int):
 	node.position.y = abs(height_diff)
 
 func _on_child_visibility_change() -> void:
-	print("_on_child_visibility_change")
 	adjust_separation()
 	_on_sort_children()
